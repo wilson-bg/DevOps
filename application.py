435626d017028ba61ccb00a3542a4f7fc48152da
wilson-bg <w.beltran@uniandes.edu.co>
2022-04-10 11:39:20 -0500

    change psycopg2

--- application.py
+++ application.py
@@ -7,27 +7,27 @@ from src.model.model import Base
 
 # Instancia de la aplicación en Flask
 #app = Flask(__name__)
-app = create_app('default')
+application = create_app('default')
 #app_context = app.app_context()
 #app_context.push()
 
-app.config.from_object('config.default_settings.Config')
-app.config.from_envvar('APPLICATION_SETTINGS', True)
-app.config.from_envvar('APPLICATION_SECRETS', True)
+application.config.from_object('config.default_settings.Config')
+application.config.from_envvar('APPLICATION_SETTINGS', True)
+application.config.from_envvar('APPLICATION_SECRETS', True)
 
 
-with app.app_context():
+with application.app_context():
     from src.connections.db_connection import db
     db.Model = Base
 
 from src.dependencies import configure
 from src.api import blacklists_api,home_api
-app.register_blueprint(home_api, url_prefix='/')
-app.register_blueprint(blacklists_api, url_prefix='/blacklists')
+application.register_blueprint(home_api, url_prefix='/')
+application.register_blueprint(blacklists_api, url_prefix='/blacklists')
 
 
 # Agregamos el inyector de dependencias 
-FlaskInjector(app=app, modules=[configure])
+FlaskInjector(app=application, modules=[configure])
 
 # Punto de arranque: gunicorn
 #def gunicorn():
@@ -38,15 +38,15 @@ FlaskInjector(app=app, modules=[configure])
     # Retornar el objeto de la aplicacion
     #return app
 
-jwt = JWTManager(app)
+jwt = JWTManager(application)
 
 # Punto de arranque: servidor de desarrollo
 print(__name__)
 if __name__ == "__main__":
     # Iniciar la base de datos si no existe
-    with app.app_context():
+    with application.app_context():
         instantiate_db(db=db)
 
-    app.run(
+    application.run(
         host="0.0.0.0", port=5000, debug=True
     )
